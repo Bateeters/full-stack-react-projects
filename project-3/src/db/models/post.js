f8053a3af5667356da526d4a5af4590d504ec876
Brian Teeters <BrianTeetersDesign@gmail.com>
2026-01-20 18:46:14 -0500

    Added test post to database

--- project-3/src/db/models/post.js
+++ project-3/src/db/models/post.js
@@ -6,10 +6,11 @@ const postSchema = new Schema({
   // We have a required title, an author, and contents which are all strings.
   title: { type: String, required: true },
   author: String,
-  content: String,
+  contents: String,
   // We also have tags, which are a string array
   tags: [String],
 })
 
 // With our schema now defined, we can create a Mongoose model
+// The first argument to .model() is the name of the collection - in this case it will be posts because we specified post (it always turns plurl)
 export const Post = mongoose.model('post', postSchema)
--- project-3/src/example.js
+++ project-3/src/example.js
@@ -1,3 +1,23 @@
 import { initDatabase } from './db/init.js'
+import { Post } from './db/models/post.js'
 
-initDatabase()
+// initDatabase is an async function so we need to await it
+// This makes sure we are connected to the database before trying to access it
+await initDatabase()
+
+// Create new blog post
+const post = new Post({
+  title: 'Hello Mongoose!',
+  author: 'Brian Teeters',
+  contents: 'This post is stored in a MongoDB database using Mongoose.',
+  tags: ['mongoose', 'mongodb'],
+})
+
+// Call .save() on the post to save it to the database
+await post.save()
+
+// Use .find() to list all posts, and log result
+const posts = await Post.find()
+console.log(posts)
+
+// Run "node src/example.js" in the console to see it run
