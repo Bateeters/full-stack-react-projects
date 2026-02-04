9acfec4808340e3bfa1c3f5c6b2846e9b7c1530b
Brian Teeters <BrianTeetersDesign@gmail.com>
2026-02-03 19:16:51 -0500

    Post Component Created

---
+++ project-4/src/components/Post.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types'
+
+// Post component that accepts a title, contents, and author
+export function Post({ title, contents, author }) {
+  return (
+    <article>
+      <h3>{title}</h3>
+      <div>{contents}</div>
+      {author && (
+        <em>
+          <br />
+          Written by <strong>{author}</strong>
+        </em>
+      )}
+    </article>
+  )
+}
+
+// Defining propTypes making sure title is required
+// propTypes are used to validate the props passed to React components when using JS
+Post.propTypes = {
+  title: PropTypes.string.isRequired,
+  contents: PropTypes.string,
+  author: PropTypes.string,
+}
