b3f86ef77b86251f80ffcf4cc2472f3a77401dae
Brian Teeters <BrianTeetersDesign@gmail.com>
2026-02-05 21:02:19 -0500

    backend posts are now populating in UI

--- project-4/src/Blog.jsx
+++ project-4/src/Blog.jsx
@@ -1,20 +1,20 @@
+import { useQuery } from '@tanstack/react-query'
 import { PostList } from './components/PostList.jsx'
 import { CreatePost } from './components/CreatePost.jsx'
 import { PostFilter } from './components/PostFilter.jsx'
 import { PostSorting } from './components/PostSorting.jsx'
-
-const posts = [
-  {
-    title: 'Full-Stack React Projects',
-    contents: "Let's dive into full-stack development!",
-    author: 'Brian Teeters',
-  },
-  {
-    title: 'Hello React!',
-  },
-]
+import { getPosts } from './api/posts.js'
 
 export function Blog() {
+  const postsQuery = useQuery({
+    // queryKey is extremely important for TanStack Query, it's used to uniquely identify a request
+    queryKey: ['posts'],
+    queryFn: () => getPosts(),
+  })
+
+  // get the posts from the query. fall back to an empty array if no posts are loaded yet
+  const posts = postsQuery.data ?? []
+
   return (
     <div style={{ padding: 15 }}>
       <CreatePost />
