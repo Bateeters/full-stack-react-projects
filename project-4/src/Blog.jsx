696a655392b36d6432f67e91c73901060af1af0e
Brian Teeters <BrianTeetersDesign@gmail.com>
2026-02-07 14:21:01 -0500

    implemented the filter and sorting

--- project-4/src/Blog.jsx
+++ project-4/src/Blog.jsx
@@ -4,12 +4,18 @@ import { CreatePost } from './components/CreatePost.jsx'
 import { PostFilter } from './components/PostFilter.jsx'
 import { PostSorting } from './components/PostSorting.jsx'
 import { getPosts } from './api/posts.js'
+import { useState } from 'react'
 
 export function Blog() {
+  // Add state hooks for the author filter and sorting options
+  const [author, setAuthor] = useState('')
+  const [sortBy, setSortBy] = useState('createdAt')
+  const [sortOrder, setSortOrder] = useState('descending')
+
   const postsQuery = useQuery({
     // queryKey is extremely important for TanStack Query, it's used to uniquely identify a request
-    queryKey: ['posts'],
-    queryFn: () => getPosts(),
+    queryKey: ['posts', { author, sortBy, sortOrder }],
+    queryFn: () => getPosts({ author, sortBy, sortOrder }),
   })
 
   // get the posts from the query. fall back to an empty array if no posts are loaded yet
@@ -20,8 +26,18 @@ export function Blog() {
       <CreatePost />
       <hr />
       Filter by:
-      <PostFilter field='author' />
-      <PostSorting fields={['createdAt', 'updatedAt']} />
+      <PostFilter
+        field='author'
+        value={author}
+        onChange={(value) => setAuthor(value)}
+      />
+      <PostSorting
+        fields={['createdAt', 'updatedAt']}
+        value={sortBy}
+        onChange={(value) => setSortBy(value)}
+        orderValue={sortOrder}
+        onOrderChange={(orderValue) => setSortOrder(orderValue)}
+      />
       <hr />
       <PostList posts={posts} />
     </div>
