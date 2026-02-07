b3fe885784f56be757fb65aa503ec45f76c67e37
Brian Teeters <BrianTeetersDesign@gmail.com>
2026-02-07 14:44:16 -0500

    implemented post creation

--- project-4/src/api/posts.js
+++ project-4/src/api/posts.js
@@ -5,3 +5,16 @@ export const getPosts = async (queryParams) => {
   )
   return await res.json()
 }
+
+// Func that accepts a post as an argument
+export const createPost = async (post) => {
+  // Make a request to the /posts endpoint
+  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/posts`, {
+    // Let the backend know we are sending a JSON body and to then send our post object as a JSON string
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(post),
+  })
+  // We also need to parse the response as JSON
+  return await res.json()
+}
--- project-4/src/components/CreatePost.jsx
+++ project-4/src/components/CreatePost.jsx
@@ -1,16 +1,62 @@
+import { useMutation } from '@tanstack/react-query'
+import { useState } from 'react'
+import { createPost } from '../api/posts.js'
+
 export function CreatePost() {
+  // define state hooks for title, author, and contents
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [contents, setContents] = useState('')
+
+  // Define a mutation hook, calling the createPost function
+  const createPostMutation = useMutation({
+    mutationFn: () => createPost({ title, author, contents }),
+  })
+
+  // Define a handleSubmit function to prevent default (page refresh) and execute the mutation using .mutate()
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    createPostMutation.mutate()
+  }
+
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
+    <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor='create-title'>Title: </label>
-        <input type='text' name='create-title' id='create-title' />
+        <input
+          type='text'
+          name='create-title'
+          id='create-title'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
       </div>
       <div>
         <label htmlFor='create-author'>Author: </label>
-        <input type='text' name='create-author' id='create-author' />
+        <input
+          type='text'
+          name='create-author'
+          id='create-author'
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
       </div>
-      <textarea />
-      <input type='submit' value='Create' />
+      <textarea
+        value={contents}
+        onChange={(e) => setContents(e.target.value)}
+      />
+      <input
+        type='submit'
+        // make the button say 'Creating...' if mutation is in progress
+        value={createPostMutation.isPending ? 'Creating...' : 'Create'}
+        // disable the 'Create' button if title is not filled or if mutation is in progress
+        disabled={!title || createPostMutation.isPending}
+      />
+
+      {
+        // Message below create button if mutation is successfull
+        createPostMutation.isSuccess ? <>Post Created Successfully!</> : null
+      }
     </form>
   )
 }
